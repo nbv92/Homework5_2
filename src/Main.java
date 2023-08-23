0bcccd6983f9be3aa477b561eb8f51d6ee517781
Timofey <timofey.sharipov@gmail.com>
2023-08-23 16:02:00 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -54,6 +54,9 @@ public class Main {
 
     public static void task4() {
         System.out.println("Task4");
-        System.out.println("");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length; i <= 0; i--) {
+            System.out.println(reverseFullName[i]);
+        }
     }
 }
